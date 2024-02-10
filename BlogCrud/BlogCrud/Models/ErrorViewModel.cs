957f31ebd0aa90eefe51a3d74cb925b4c2dd000e
sahbaz khan <khansahbaz61@gmail.com>
2024-02-10 21:01:09 +0530

    created project

---
+++ BlogCrud/BlogCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
